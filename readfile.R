8d1dffb6abb3f74cc9d9ab4c265715408b80fdf8
Bianca Rosa <me@biancarosa.com.br>
2016-07-02 13:31:14 -0300

    Plot first histogram

---
+++ readfile.R
@@ -0,0 +1,17 @@
+
+setwd("/home/bianca/R/exploratory-data-analysis/ExData_Plotting1/data")
+zip_file_name <- "exdata_data_household_power_consumption.zip"
+if (!file.exists(zip_file_name)) { 
+  file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(file_url, destfile = zip_file_name, method='curl')
+  unzip (zip_file_name)
+}
+txt_file_name = "household_power_consumption.txt"
+hpc <- read.csv(txt_file_name,sep = ";", stringsAsFactors=FALSE, dec=".")
+setwd("/home/bianca/R/exploratory-data-analysis/ExData_Plotting1")
+
+hpc[, "Global_active_power"] <- as.numeric(hpc[, "Global_active_power"])
+hpc[, "Date"] <- as.Date(hpc[,"Date"], "%d/%m/%Y")
+from = as.Date("2007-02-01")
+to = as.Date("2007-02-02")
+subset <- hpc[hpc$Date %in% from:to, ]
